f78234a152e7fa19d8d061e30f4a695bb95b36d1
Hassan Jawhar <hassan-jawhar@hotmail.com>
2016-05-17 21:24:10 +0300

    Update

--- project/config.php
+++ project/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $dbhost = "localhost";
-$dbusername = "wwwhqbju_project";
-$dbpassword = "st37CWna2f22K3auPS";
-$dbname = "wwwhqbju_project";
+$dbusername = "dbuser";
+$dbpassword = "password";
+$dbname = "dbname";
 $conn = mysqli_connect($dbhost, $dbusername, $dbpassword);
 if ($conn) {
     mysqli_select_db($conn, $dbname);

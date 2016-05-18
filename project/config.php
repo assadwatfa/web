bd821414e4afe43d036b6ed0710fa71debe09a28
Hassan Jawhar <hassan-jawhar@hotmail.com>
2016-05-18 18:28:33 +0300

    Update

--- project/config.php
+++ project/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $dbhost = "localhost";
-$dbusername = "dbuser";
-$dbpassword = "password";
-$dbname = "dbname";
+$dbusername = "";
+$dbpassword = "";
+$dbname = "";
 $conn = mysqli_connect($dbhost, $dbusername, $dbpassword);
 if ($conn) {
     mysqli_select_db($conn, $dbname);
